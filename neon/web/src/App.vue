9d04b200db162d5795eddcbec89b84794d62d44d
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-03-05 19:15:40 +0100

    Add vue application distributed as umd with vite

---
+++ neon/web/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card-tile">
+    <p>4programmers » Praca</p>
+    <h2>
+      Oferty Pracy w IT - Odwiedza nas ponad 150 tys. programistów miesięcznie
+    </h2>
+  </div>
+  <div v-for="jobOffer in props.jobOfferTitles" class="card-tile">
+    {{ jobOffer }}
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  jobOfferTitles: string[];
+}
+
+const props = defineProps<Props>();
+</script>
