429bbfe718d374d78eea2aaaf599fba5f0b1517c
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-07-18 19:17:32 +0200

    Add subitems to navigation elements for candidate and employer in desktop

--- neon2/web/src/Infrastructure/Vue/NavigationView/View/Presenter/entryPointItems.ts
+++ neon2/web/src/Infrastructure/Vue/NavigationView/View/Presenter/entryPointItems.ts
@@ -1,8 +1,24 @@
 import {NavigationAction} from "../../Port/NavigationService";
 
 export const entryPointItems: EntryPointItem[] = [
-  {type: 'link', title: 'Dla kandydatów', action: 'jobBoard'},
-  {type: 'link', title: 'Dla pracodawców', action: 'pricing'},
+  {
+    type: 'link',
+    title: 'Dla kandydatów',
+    action: 'jobBoard',
+    children: [
+      {title: 'Oferty Pracy IT', action: 'jobBoard'},
+      {title: 'Opinie o Pracodawcach', action: 'employerReviews'},
+    ],
+  },
+  {
+    type: 'link',
+    title: 'Dla pracodawców',
+    action: 'pricing',
+    children: [
+      {title: 'Cennik Ogłoszeń', action: 'pricing'},
+      {title: 'Stwórz darmowy profil', action: 'register'},
+    ],
+  },
   {type: 'link', title: 'Forum', action: 'forum', forumMenu: true},
   {type: 'link', title: 'Mikroblogi', action: 'blog'},
 ];
@@ -12,4 +28,10 @@ export interface EntryPointItem {
   title: string;
   action: NavigationAction;
   forumMenu?: boolean;
+  children?: NavigationItem[];
+}
+
+export interface NavigationItem {
+  title: string;
+  action: NavigationAction;
 }
