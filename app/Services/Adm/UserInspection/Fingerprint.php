15ad9e4915f16af1775a481dbf671f383995f241
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-08-07 15:22:48 +0200

    Add href to user fingerprint in administrator panel

--- app/Services/Adm/UserInspection/Fingerprint.php
+++ app/Services/Adm/UserInspection/Fingerprint.php
@@ -2,9 +2,17 @@
 namespace Coyote\Services\Adm\UserInspection;
 
 readonly class Fingerprint {
+    public string $fingerprintHref;
+
     public function __construct(
         public string $fingerprint,
         public int    $timesUsed,
         public string $lastUsed,
-    ) {}
+    ) {
+        $this->fingerprintHref = $this->fingerprintHref($this->fingerprint);
+    }
+
+    private function fingerprintHref(string $fingerprint): string {
+        return route('adm.stream') . '?' . http_build_query(['fingerprint' => $fingerprint]);
+    }
 }
--- app/View/FingerprintUserMentionTableItem.php
+++ app/View/FingerprintUserMentionTableItem.php
@@ -11,16 +11,22 @@ readonly class FingerprintUserMentionTableItem {
      */
     public function __construct(
         public string $fingerprint,
+        public string $fingerprintHref,
         public array  $users,
     ) {}
 
     public static function from(FingerprintUserTableItem $item): self {
         return new self(
             $item->fingerprint,
+            self::fingerprintHref($item->fingerprint),
             \array_map(self::userMention(...), $item->users));
     }
 
     private static function userMention(UserInspection\User $user): Mention {
         return new Mention($user->userId, $user->username);
+    }
+
+    private static function fingerprintHref(string $fingerprint): string {
+        return route('adm.stream') . '?' . http_build_query(['fingerprint' => $fingerprint]);
     }
 }
