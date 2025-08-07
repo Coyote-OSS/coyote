24328668da4660255fb74921569d4f84e3ecfb3d
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-08-07 14:39:20 +0200

    Correct PrivacyControllerTest

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,15 +8,17 @@ use Illuminate\Database\Eloquent\Factory;
 /** @var Factory $factory */
 $factory->define(\Coyote\User::class, function (Faker $faker) {
     return [
-        'name'            => $faker->userName . $faker->randomNumber(3),
-        'email'           => $faker->unique()->safeEmail,
-        'password'        => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
-        'is_confirm'      => true,
-        'alert_login'     => true,
-        'guest_id'        => $faker->uuid,
-        'allow_subscribe' => true,
-        'visited_at'      => now(),
-        'sig'             => 'Lorem ipsum dolor sit amet.',
+        'name'                 => $faker->userName . $faker->randomNumber(3),
+        'email'                => $faker->unique()->safeEmail,
+        'password'             => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
+        'is_confirm'           => true,
+        'alert_login'          => true,
+        'guest_id'             => $faker->uuid,
+        'allow_subscribe'      => true,
+        'visited_at'           => now(),
+        'sig'                  => 'Lorem ipsum dolor sit amet.',
+        'pm_unread'            => 0,
+        'notifications_unread' => 0,
     ];
 });
 
