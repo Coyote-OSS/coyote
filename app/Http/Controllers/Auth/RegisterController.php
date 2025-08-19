14f1074d51c169213b9a1c6480bb08910ead0773
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-08-19 12:09:10 +0200

    After registration, redirect user to the original page the user was on

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Coyote\Http\Controllers\Auth;
 
 use Carbon\Carbon;
@@ -9,44 +8,39 @@ use Coyote\Events\UserSaved;
 use Coyote\Http\Controllers\Controller;
 use Coyote\Http\Forms\Auth\RegisterForm;
 use Coyote\Mail\UserRegistered;
-use Coyote\Services\FormBuilder\Form;
 use Coyote\Services\Stream\Activities\Create as Stream_Create;
 use Coyote\Services\Stream\Objects\Person as Stream_Person;
 use Coyote\User;
 use Illuminate\Contracts\Mail\MailQueue;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Jenssegers\Agent\Agent;
 
-class RegisterController extends Controller
-{
-    public function __construct()
-    {
+class RegisterController extends Controller {
+    public function __construct() {
         parent::__construct();
         $this->middleware('guest');
     }
 
-    public function index(RouteVisits $visits): View
-    {
+    public function index(RouteVisits $visits, Request $request): View {
         $this->breadcrumb->push('Rejestracja', route('register'));
         $agent = new Agent();
         if (!$agent->isRobot($this->request->userAgent())) {
             $visits->visit($this->request->path(), Carbon::now()->toDateString());
         }
-        return $this->view('auth.register', ['form' => $this->form()]);
+        return $this->view('auth.register', [
+            'form'       => $this->createForm(RegisterForm::class, null, [
+                'url' => route('register'),
+            ]),
+            'refererUrl' => $request->headers->get('referer'),
+        ]);
     }
 
-    private function form(): Form
-    {
-        return $this->createForm(RegisterForm::class, null, ['url' => route('register')]);
-    }
-
-    public function signup(RegisterForm $form): RedirectResponse
-    {
+    public function signup(RegisterForm $form): RedirectResponse {
         $request = $form->getRequest();
-
         $this->transaction(function () use ($request) {
-            $user = User::forceCreate([
+            $user = User::query()->forceCreate([
                 'name'                 => $request->input('name'),
                 'email'                => $request->input('email'),
                 'password'             => bcrypt($request->input('password')),
@@ -55,21 +49,15 @@ class RegisterController extends Controller
                 'newsletter_agreement' => true,
                 'allow_smilies'        => true,
             ]);
-
             app(MailQueue::class)
                 ->to($request->input('email'))
                 ->send(new UserRegistered(Actkey::createLink($user->id)));
-
-            auth()
-                ->login($user, true);
-
+            auth()->login($user, true);
             stream(Stream_Create::class, new Stream_Person());
-
             event(new UserSaved($user));
         });
-
         return redirect()
-            ->intended(route('user.home'))
+            ->to($request->input('refererUrl'))
             ->with('success', 'Konto zostało utworzone. Na podany adres e-mail, przesłany został link aktywacyjny.');
     }
 }
