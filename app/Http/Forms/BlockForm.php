80c156a7eb160f016a4277b1f15d56f790f656e7
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-05-13 10:46:04 +0200

    Warn user wanting to add <script> into the static block, regaring cloudflare xss-protection

--- app/Http/Forms/BlockForm.php
+++ app/Http/Forms/BlockForm.php
@@ -5,65 +5,66 @@ namespace Coyote\Http\Forms;
 use Coyote\Services\FormBuilder\Form;
 use Coyote\Services\FormBuilder\ValidatesWhenSubmitted;
 
-class BlockForm extends Form implements ValidatesWhenSubmitted
-{
+class BlockForm extends Form implements ValidatesWhenSubmitted {
     protected $theme = self::THEME_INLINE;
 
-    public function buildForm()
-    {
+    public function buildForm() {
         $this
             ->add('name', 'text', [
                 'rules' => 'required|string|max:40',
                 'label' => 'Nazwa bloku',
             ])
             ->add('region', 'select', [
-                'label' => 'Region',
-                'choices' => $this->regions(),
-                'empty_value' => '--'
+                'label'       => 'Region',
+                'choices'     => $this->regions(),
+                'empty_value' => '--',
             ])
             ->add('content', 'textarea', [
                 'label' => 'Kod HTML',
                 'rules' => 'required|string',
-                'attr' => [
-                    'id' => 'code'
-                ]
+                'attr'  => [
+                    'id' => 'code',
+                ],
+                'help'  => \htmlEntities(
+                    'Dodanie tagów <script> może wymagać tymczasowego wyłączenia ' .
+                    'zabezpieczeń Cloudflare (np. "detection tools", "managed ruleset", "waf", "xss", "app exploit").',
+                ),
             ])
             ->add('is_enabled', 'checkbox', [
-                'label' => 'Włączony'
+                'label' => 'Włączony',
             ])
             ->add('enable_sponsor', 'checkbox', [
-                'label' => 'Włączony dla sponsorów'
+                'label' => 'Włączony dla sponsorów',
             ])
             ->add('max_reputation', 'text', [
                 'label' => 'Wyświetlaj tylko użytkownikom z reputacją od 0 do...',
-                'help' => 'Blok ten nie będzie wyświetlany użytkownikom, którzy posiadają więcej niż N punktów reputacji',
+                'help'  => 'Blok ten nie będzie wyświetlany użytkownikom, którzy posiadają więcej niż N punktów reputacji',
                 'rules' => 'nullable|integer|min:1',
-                'attr' => [
-                    'style' => 'width: 50px'
-                ]
+                'attr'  => [
+                    'style' => 'width: 50px',
+                ],
             ])
             ->add('submit', 'submit_with_delete', [
-                'label' => 'Zapisz',
-                'attr' => [
-                    'data-submit-state' => 'Zapisywanie...'
+                'label'             => 'Zapisz',
+                'attr'              => [
+                    'data-submit-state' => 'Zapisywanie...',
                 ],
-                'delete_url' => empty($this->data->id) ? '' : route('adm.blocks.delete', [$this->data->id]),
-                'delete_visibility' => !empty($this->data->id)
+                'delete_url'        => empty($this->data->id) ? '' : route('adm.blocks.delete', [$this->data->id]),
+                'delete_visibility' => !empty($this->data->id),
             ]);
     }
 
     /**
      * @return array
      */
-    private function regions()
-    {
+    private function regions() {
         return [
-            'header' => 'Nagłówek',
-            'footer' => 'Stopka',
-            'head' => 'Znacznik <head>',
-            'body' => 'Znacznik <body>',
+            'header'      => 'Nagłówek',
+            'footer'      => 'Stopka',
+            'head'        => 'Znacznik <head>',
+            'body'        => 'Znacznik <body>',
             'wiki_footer' => 'Stopka artykułów',
-            'bottom' => 'Koniec strony'
+            'bottom'      => 'Koniec strony',
         ];
     }
 }
--- app/Http/Grids/Adm/BlockGrid.php
+++ app/Http/Grids/Adm/BlockGrid.php
@@ -2,55 +2,51 @@
 
 namespace Coyote\Http\Grids\Adm;
 
+use Boduch\Grid\Components\EditButton;
 use Boduch\Grid\Decorators\Boolean;
 use Boduch\Grid\Decorators\FormatDateRelative;
+use Boduch\Grid\Order;
 use Coyote\Block;
 use Coyote\Services\Grid\Components\CreateButton;
 use Coyote\Services\Grid\Grid;
-use Boduch\Grid\Order;
-use Boduch\Grid\Components\EditButton;
 
-class BlockGrid extends Grid
-{
-    public function buildGrid()
-    {
+class BlockGrid extends Grid {
+    public function buildGrid() {
         $this
             ->setDefaultOrder(new Order('updated_at', 'desc'))
             ->addColumn('id', [
-                'title' => 'ID',
-                'sortable' => true,
+                'title'     => 'ID',
+                'sortable'  => true,
                 'clickable' => function (Block $row) {
                     return link_to_route('adm.blocks.save', $row->id, [$row->id]);
-                }
+                },
             ])
             ->addColumn('name', [
-                'title' => 'Nazwa bloku',
+                'title'     => 'Nazwa bloku',
                 'clickable' => function (Block $row) {
                     return link_to_route('adm.blocks.save', $row->name, [$row->id]);
-                }
+                },
             ])
             ->addColumn('created_at', [
-                'title' => 'Data utworzenia',
-                'sortable' => true,
-                'decorators' => [new FormatDateRelative('nigdy')]
+                'title'      => 'Data utworzenia',
+                'sortable'   => true,
+                'decorators' => [new FormatDateRelative('nigdy')],
             ])
             ->addColumn('updated_at', [
-                'title' => 'Data modyfikacji',
-                'sortable' => true,
-                'decorators' => [new FormatDateRelative('nigdy')]
+                'title'      => 'Data modyfikacji',
+                'sortable'   => true,
+                'decorators' => [new FormatDateRelative('nigdy')],
             ])
             ->addColumn('is_enabled', [
-                'title' => 'Włączony',
-                'decorators' => [new Boolean()]
+                'title'      => 'Włączony',
+                'decorators' => [new Boolean()],
             ])
             ->addRowAction(new EditButton(function (Block $row) {
                 return route('adm.blocks.save', [$row->id]);
             }))
-            ->addComponent(
-                new CreateButton(
-                    route('adm.blocks.save'),
-                    'Nowy blok'
-                )
-            );
+            ->addComponent(new CreateButton(
+                route('adm.blocks.save'),
+                'Nowy blok',
+            ));
     }
 }
