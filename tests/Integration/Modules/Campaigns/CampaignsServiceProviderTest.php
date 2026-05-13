56901b7e45e6e1800820df111d75a75729219100
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-05-13 13:15:11 +0200

    Display campaign banners in homepage and topic page

--- tests/Integration/Modules/Campaigns/CampaignsServiceProviderTest.php
+++ tests/Integration/Modules/Campaigns/CampaignsServiceProviderTest.php
@@ -3,6 +3,7 @@ namespace Tests\Integration\Modules\Campaigns;
 
 use Coyote\Modules\Campaigns\AuthPriviligedUsers;
 use Coyote\Modules\Campaigns\CampaignsServiceProvider;
+use Modules\Campaigns;
 use Modules\Campaigns\ForPriviligedUsers;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
@@ -17,5 +18,14 @@ class CampaignsServiceProviderTest extends TestCase {
     public function serviceProviderRegisteresAuthPriviligedUsers(): void {
         $users = $this->laravel->app->get(ForPriviligedUsers::class);
         $this->assertInstanceOf(AuthPriviligedUsers::class, $users);
+    }
+
+    #[Test]
+    public function hardcodedCampaignBanners(): void {
+        /** @var Campaigns\Campaigns $campaigns */
+        $campaigns = $this->laravel->app->get(Campaigns\Campaigns::class);
+        $banners = $campaigns->campaignBanners();
+        $this->assertEquals('foo.png', $banners->sidebar);
+        $this->assertEquals(['bar.png'], $banners->horizontal);
     }
 }
