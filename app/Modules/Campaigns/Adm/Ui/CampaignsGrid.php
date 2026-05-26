052dde82d549c087b0c79c0bf03b28c545c91218
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-05-26 13:36:35 +0200

    Add campaign active cell in campaign grid

--- app/Modules/Campaigns/Adm/Ui/CampaignsGrid.php
+++ app/Modules/Campaigns/Adm/Ui/CampaignsGrid.php
@@ -6,11 +6,15 @@ use Boduch\Grid\Components\ShowButton;
 use Boduch\Grid\Decorators\CodeText;
 use Boduch\Grid\Decorators\LongText;
 use Boduch\Grid\Order;
+use Coyote\Domain\Html;
+use Coyote\Domain\Icon\Icons;
 use Coyote\Modules\Campaigns\Eloquent\Campaign;
 use Coyote\Services\Grid\Grid;
+use Modules\Campaigns\CampaignService;
 
 class CampaignsGrid extends Grid {
     public function buildGrid(): void {
+        $campaigns = $this->campaignsService();
         $this
             ->setDefaultOrder(new Order('id', 'desc'))
             ->addColumn('id', [
@@ -29,11 +33,30 @@ class CampaignsGrid extends Grid {
                 'title'      => 'URL przekierowania',
                 'decorators' => [new LongText()],
             ])
+            ->addColumn('is_active', [
+                'title'  => 'Aktywna',
+                'render' => fn(Campaign $campaign) => $this->campaignActiveCell($campaigns, $campaign),
+            ])
             ->addRowAction(new ShowButton(function (Campaign $row) {
                 return route('adm.campaigns.show', [$row->id]);
             }))
             ->addRowAction(new EditButton(function (Campaign $row) {
                 return route('adm.campaigns.save', [$row->id]);
             }));
+    }
+
+    private function campaignsService(): CampaignService {
+        return app(CampaignService::class);
+    }
+
+    private function campaignActiveCell(CampaignService $campaigns, Campaign $campaign): string {
+        $active = $campaigns->campaignActive($campaign->campaign_key);
+        $icon = $this->icon($active ? 'campaignStatusActive' : 'campaignStatusInactive');
+        $title = $active ? 'aktywna' : 'nie aktywna';
+        return "$icon $title";
+    }
+
+    private function icon(string $iconName): Html {
+        return new Icons()->icon($iconName);
     }
 }
