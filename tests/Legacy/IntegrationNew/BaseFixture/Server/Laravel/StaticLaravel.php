6128e40e8a290fc078a774627452dbaf5723026a
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-06-25 12:09:58 +0200

    Update "phpunit/phpunit" to "^13.2.1"

--- tests/Legacy/IntegrationNew/BaseFixture/Server/Laravel/StaticLaravel.php
+++ tests/Legacy/IntegrationNew/BaseFixture/Server/Laravel/StaticLaravel.php
@@ -5,61 +5,52 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\Legacy\IntegrationNew\BaseFixture\Server\Laravel;
 
-class StaticLaravel
-{
+class StaticLaravel {
     private static ?Laravel\TestCase $laravel = null;
 
-    public static function get(\PHPUnit\Framework\TestCase $testCase): ?Laravel\TestCase
-    {
+    public static function get(\PHPUnit\Framework\TestCase $testCase): ?Laravel\TestCase {
         if (!self::isLaravelApplication($testCase)) {
             return null;
         }
         if (self::$laravel === null) {
-            $transactional = self::isTransactional($testCase);
-            self::$laravel = self::getInstance($testCase->name(), $transactional, self::bootTest($testCase));
+            self::$laravel = self::getInstance($testCase->name(), self::isTransactional($testCase));
+            self::$laravel->beforeBoot = self::bootTest($testCase);
             self::$laravel->setUp();
         }
         return self::$laravel;
     }
 
-    private static function getInstance(string $name, bool $transactional, callable $beforeBoot): Laravel\TestCase
-    {
+    private static function getInstance(string $name, bool $transactional): Laravel\TestCase {
         if ($transactional) {
-            return new class ($name, $beforeBoot) extends Laravel\TestCase {
+            return new class ($name) extends Laravel\TestCase {
                 use DatabaseTransactions;
             };
         }
-        return new Laravel\TestCase($name, $beforeBoot);
+        return new Laravel\TestCase($name);
     }
 
-    public static function destroy(): void
-    {
+    public static function destroy(): void {
         self::$laravel?->tearDown();
         self::$laravel = null;
     }
 
-    private static function isLaravelApplication($testCase): bool
-    {
+    private static function isLaravelApplication($testCase): bool {
         return self::hasMarker($testCase, Laravel\Application::class);
     }
 
-    private static function isTransactional($testCase): bool
-    {
+    private static function isTransactional($testCase): bool {
         return self::hasMarker($testCase, Laravel\Transactional::class);
     }
 
-    private static function hasMarker($testCase, string $markerClass): bool
-    {
+    private static function hasMarker($testCase, string $markerClass): bool {
         return \in_array($markerClass, \class_uses_recursive($testCase));
     }
 
-    private static function bootTest(\PHPUnit\Framework\TestCase $testCase): \Closure
-    {
+    private static function bootTest(\PHPUnit\Framework\TestCase $testCase): \Closure {
         return fn(Application $app) => self::beforeBoot($testCase, $app);
     }
 
-    private static function beforeBoot(\PHPUnit\Framework\TestCase $testCase, Application $app): void
-    {
+    private static function beforeBoot(\PHPUnit\Framework\TestCase $testCase, Application $app): void {
         /**
          * In order to polymorphically call {@see Test::beforeBoot},
          * there would have to be a feature in PhpUnit which allows
