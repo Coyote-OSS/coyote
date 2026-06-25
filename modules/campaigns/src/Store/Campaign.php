e2ac7708460457e6666310cde309c6346036287e
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-06-25 15:45:13 +0200

    Refactor, move .variantsOfType() to Campaign

--- modules/campaigns/src/Store/Campaign.php
+++ modules/campaigns/src/Store/Campaign.php
@@ -1,6 +1,8 @@
 <?php
 namespace Modules\Campaigns\Store;
 
+use Modules\Campaigns\VariantType;
+
 readonly class Campaign {
     /**
      * @param CampaignVariant[] $variants
@@ -10,4 +12,13 @@ readonly class Campaign {
         public CampaignPayload $payload,
         public array           $variants,
     ) {}
+
+    /**
+     * @return CampaignVariant[]
+     */
+    public function variantsOfType(VariantType $type): array {
+        return \array_values(array_filter(
+            $this->variants,
+            fn($variant) => $variant->payload->type === $type));
+    }
 }
