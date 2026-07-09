b7b44b80a9016bb2474e4014346b5c60746f25a7
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-07-09 16:02:08 +0200

    Add {title} option to {{ icon() }} function

--- app/Services/TwigBridge/Extensions/Icon.php
+++ app/Services/TwigBridge/Extensions/Icon.php
@@ -6,24 +6,23 @@ use Coyote\Domain\Icon\Icons;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
-class Icon extends AbstractExtension
-{
-    private Icons $icons;
+class Icon extends AbstractExtension {
+    private readonly Icons $icons;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->icons = new Icons();
     }
 
-    public function getFunctions(): array
-    {
+    public function getFunctions(): array {
         return [new TwigFunction('icon', $this->icon(...))];
     }
 
-    private function icon(string $iconName, array $options = []): Html
-    {
+    private function icon(string $iconName, array $options = []): Html {
         if (\array_key_exists('spin', $options)) {
             return $this->icons->iconSpin($iconName);
+        }
+        if (\array_key_exists('title', $options)) {
+            return $this->icons->iconTitle($iconName, $options['title']);
         }
         return $this->icons->icon($iconName);
     }
