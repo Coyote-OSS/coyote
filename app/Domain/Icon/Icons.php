f90440d58938c57c596750b691e1289f5b970f05
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-07-09 16:02:08 +0200

    Add {title} option to {{ icon() }} function

--- app/Domain/Icon/Icons.php
+++ app/Domain/Icon/Icons.php
@@ -4,38 +4,42 @@ namespace Coyote\Domain\Icon;
 use Coyote\Domain\Html;
 use Coyote\Domain\StringHtml;
 
-readonly class Icons
-{
+readonly class Icons {
     private FontAwesomePro $fa;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->fa = new FontAwesomePro();
     }
 
-    public function icon(string $iconName): Html
-    {
-        return $this->iconWithClass($iconName, 'fa-fw');
+    public function icon(string $iconName): Html {
+        return $this->iconWithClass($iconName, 'fa-fw', null);
     }
 
-    public function iconSpin(string $iconName): Html
-    {
-        return $this->iconWithClass($iconName, 'fa-fw fa-spin');
+    public function iconTitle(string $iconName, string $title): Html {
+        return $this->iconWithClass($iconName, 'fa-fw', $title);
     }
 
-    private function iconWithClass(string $iconName, string $modifierClass): Html
-    {
+    public function iconSpin(string $iconName): Html {
+        return $this->iconWithClass($iconName, 'fa-fw fa-spin', null);
+    }
+
+    private function iconWithClass(
+        string  $iconName,
+        string  $modifierClass,
+        ?string $title,
+    ): Html {
         $class = $this->iconClass($iconName);
-        return $this->iconTag("$class $modifierClass", $iconName);
+        return $this->iconTag("$class $modifierClass", $iconName, $title);
     }
 
-    private function iconTag(string $class, string $iconName): Html
-    {
+    private function iconTag(string $class, string $iconName, ?string $title): Html {
+        if ($title !== null) {
+            return new StringHtml(\sPrintF('<i class="%s" data-icon="%s" title="%s"></i>', $class, $iconName, $title));
+        }
         return new StringHtml(\sPrintF('<i class="%s" data-icon="%s"></i>', $class, $iconName));
     }
 
-    private function iconClass(string $iconName): string
-    {
+    private function iconClass(string $iconName): string {
         $icons = $this->fa->icons();
         if (\array_key_exists($iconName, $icons)) {
             return $icons[$iconName];
@@ -43,8 +47,7 @@ readonly class Icons
         throw new \InvalidArgumentException("Failed to find icon: $iconName");
     }
 
-    public function icons(): array
-    {
+    public function icons(): array {
         return $this->fa->icons();
     }
 }
--- tests/Legacy/IntegrationNew/Icon/TwigExtensionTest.php
+++ tests/Legacy/IntegrationNew/Icon/TwigExtensionTest.php
@@ -9,42 +9,43 @@ use Twig\Environment;
 use Twig\Loader\ArrayLoader;
 use Twig\Runtime\EscaperRuntime;
 
-class TwigExtensionTest extends TestCase
-{
+class TwigExtensionTest extends TestCase {
     #[Test]
-    public function iconTickMark(): void
-    {
+    public function iconTickMark(): void {
         $this->assertSame(
             '<i class="fa-light fa-check fa-fw" data-icon="adminTickMark"></i>',
             $this->twig("{{ icon('adminTickMark') }}"));
     }
 
     #[Test]
-    public function iconCrossMark(): void
-    {
+    public function iconCrossMark(): void {
         $this->assertSame(
             '<i class="fa-light fa-xmark fa-fw" data-icon="adminCrossMark"></i>',
             $this->twig("{{ icon('adminCrossMark') }}"));
     }
 
     #[Test]
-    public function missingIcon(): void
-    {
+    public function missingIcon(): void {
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('An exception has been thrown during the rendering of a template');
         $this->twig("{{ icon('missing') }}");
     }
 
     #[Test]
-    public function fontAwesomeSpin(): void
-    {
+    public function fontAwesomeSpin(): void {
         $this->assertSame(
             '<i class="fa-light fa-xmark fa-fw fa-spin" data-icon="adminCrossMark"></i>',
             $this->twig("{{ icon('adminCrossMark', {spin}) }}"));
     }
 
-    private function twig(string $sourceCode): string
-    {
+    #[Test]
+    public function iconTooltip(): void {
+        $this->assertSame(
+            '<i class="fa-light fa-xmark fa-fw" data-icon="adminCrossMark" title="foo"></i>',
+            $this->twig("{{ icon('adminCrossMark', {title:'foo'}) }}"));
+    }
+
+    private function twig(string $sourceCode): string {
         $twig = new Environment(new ArrayLoader());
         $twig->getRuntime(EscaperRuntime::class)->addSafeClass(Html::class, ['html']);
         $twig->addExtension(new Icon());
