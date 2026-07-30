b10dbd7e33cc2a37b7fd328456d31323cdaf1efe
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-07-30 15:11:19 +0200

    Create VariantType from image dimensions

--- modules/campaigns/src/VariantType.php
+++ modules/campaigns/src/VariantType.php
@@ -8,4 +8,21 @@ enum VariantType {
     case StandardXl;
     case SidebarXl;
     case LeaderBoardXl;
+
+    public static function fromSize(int $width, int $height): ?VariantType {
+        return array_find(
+            VariantType::cases(),
+            fn($type) => self::widthHeight($type) === [$width, $height]);
+    }
+
+    private static function widthHeight(VariantType $type): array {
+        return match ($type) {
+            VariantType::Standard      => [728, 90],
+            VariantType::Sidebar       => [300, 250],
+            VariantType::LeaderBoard   => [1140, 90],
+            VariantType::StandardXl    => [728, 200],
+            VariantType::SidebarXl     => [300, 600],
+            VariantType::LeaderBoardXl => [1140, 200],
+        };
+    }
 }
