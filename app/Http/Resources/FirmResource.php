fa8f8174960389d11f301a0a8c248aef80a6d423
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-09-03 04:26:07 +0200

    Correct LinkTest, remove parsing image size in FirmResource

--- app/Http/Resources/FirmResource.php
+++ app/Http/Resources/FirmResource.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace Coyote\Http\Resources;
 
 use Carbon\Carbon;
 use Coyote\Services\Media\File;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
@@ -14,16 +14,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
  * @property Carbon $created_at
  * @property Carbon $updated_at
  */
-class FirmResource extends JsonResource
-{
-    /**
-     * Transform the resource into an array.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return array
-     */
-    public function toArray($request)
-    {
+class FirmResource extends JsonResource {
+    public function toArray(Request $request): array {
         $data = array_except(
             $this->resource->toArray(),
             ['slug', 'user_id', 'headline', 'employees', 'founded', 'country_id', 'vat_id', 'deleted_at', 'is_private', 'benefits', 'gallery'],
@@ -33,7 +25,6 @@ class FirmResource extends JsonResource
             'created_at' => $this->created_at->toIso8601String(),
             'updated_at' => $this->updated_at->toIso8601String(),
             'logo'       => (string)$this->logo->url(),
-            'hasLogo'    => $this->logo->getFilename() && $this->logo->size() > 0,
             'url'        => route('job.firm', $this->slug),
         ]);
     }
