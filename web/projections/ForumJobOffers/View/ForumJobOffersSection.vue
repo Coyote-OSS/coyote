330f093cb2ebbfd18612876583221588a33fc609
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-09-03 06:02:33 +0200

    Add 4programmers logo to <ForumJobOffersSection/>

--- web/projections/ForumJobOffers/View/ForumJobOffersSection.vue
+++ web/projections/ForumJobOffers/View/ForumJobOffersSection.vue
@@ -1,7 +1,8 @@
 <template>
   <section class="tile-backlight flex flex-col items-start gap-6 rounded-lg p-4 sm:p-6">
     <div class="flex w-full items-center justify-between">
-      <h2 class="text-base font-semibold text-gray-800">Oferty Pracy</h2>
+      <img :src="logoLight" alt="4programmers.net" class="h-5 dark:hidden"/>
+      <img :src="logoDark" alt="4programmers.net" class="hidden h-5 dark:block"/>
       <a :href="jobBoardHref" class="flex items-center gap-1 text-xs font-medium text-green-500 no-underline">
         Zobacz więcej
         <Icon name="chevron-right" class="text-sm"/>
@@ -19,6 +20,8 @@
 
 <script setup lang="ts">
 import Icon from '../../../libs/Icon/Icon.vue';
+import logoDark from '../../../../public/img/logo.dark.svg';
+import logoLight from '../../../../public/img/logo.light.svg';
 import type {ForumJobOfferTile} from '../ViewModel/ForumJobOfferTile';
 import VueForumJobOfferTile from './ForumJobOfferTile.vue';
 
